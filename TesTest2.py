430b8b8274acee1f3c5e9e1cf74f4a4db87a6caa
avery.liu <yingpei.liu81@gmail.com>
2024-06-15 11:41:37 +0900

    Update TesTest2.py

--- TesTest2.py
+++ TesTest2.py
@@ -22,7 +22,6 @@ class SamplePageSearch(unittest.TestCase):
         self.driver.maximize_window()
 
     # Testcase1: add 2 Abag and 1 Bbag to cart and show correct price and quantity on the page
-    # Testcase2: change Abag from 2 to 3, and show correct price and quantity on the page
     def test_cart_ADD(self):
         self.button=self.driver.find_element(By.XPATH, "/html/body/section[1]/div/div/main/div[3]/div/div/article[1]/a/div/div[1]/div/img")
         self.button.click()
@@ -52,7 +51,7 @@ class SamplePageSearch(unittest.TestCase):
 
 
 
-    #Testcase2:
+    # Testcase2: change Abag from 2 to 3, and show correct price and quantity on the page
     def test_cart_CHANGE(self):
         self.button=self.driver.find_element(By.XPATH, "/html/body/section[1]/div/div/main/div[3]/div/div/article[1]/a/div/div[1]/div/img")
         self.button.click()
@@ -73,7 +72,7 @@ class SamplePageSearch(unittest.TestCase):
         self.button=self.driver.find_element(By.NAME, "upButton")
         self.button.click()
 
-    #total price and quantity are correct
+        #total price and quantity are correct
         expect_number="¥6,400"
         actual_number=self.driver.find_element(By.XPATH,"/html/body/div[2]/main/article/form/div[1]/table/tfoot/tr/th[5]").text
             #print(actual_number)
@@ -85,7 +84,7 @@ class SamplePageSearch(unittest.TestCase):
         assert actual_quantity==expect_quantity,"expect_quantity'{expect_quantity}',but got'{actual_quantity}'"
 
 
-    #Testcase3:
+    # Testcase3: Delete 2 Abag from cart, and show correct price and quantity on the page
     def test_cart_DELETE(self):
         self.button=self.driver.find_element(By.XPATH, "/html/body/section[1]/div/div/main/div[3]/div/div/article[1]/a/div/div[1]/div/img")
         self.button.click()
@@ -101,15 +100,10 @@ class SamplePageSearch(unittest.TestCase):
         self.button=self.driver.find_element(By.ID, "inCart[10395][item28-1]")
         self.button.click()
 
-        self.driver.find_element(By.NAME, "quant[0][10395][item28]").clear()
-        self.driver.find_element(By.NAME, "quant[0][10395][item28]").send_keys("3")
-        self.button=self.driver.find_element(By.NAME, "upButton")
-        self.button.click()
-
         self.button=self.driver.find_element(By.NAME, "delButton[0][10395][item28]")
         self.button.click()
 
-    #total price and quantity are correct
+        #total price and quantity are correct
         expect_number="¥1,600"
         actual_number=self.driver.find_element(By.XPATH,"/html/body/div[2]/main/article/form/div[1]/table/tfoot/tr/th[5]").text
             #print(actual_number)
